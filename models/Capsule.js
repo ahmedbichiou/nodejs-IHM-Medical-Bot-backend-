a741745235fc6080edd05a55b843dfd39347a3af
ahmedbichiou <eunabs@outlook.fr>
2024-10-21 19:59:17 +0100

    backend updates

--- models/Capsule.js
+++ models/Capsule.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 const CapsuleSchema = new mongoose.Schema({
-  content: { type: String, required: true },
+  content: { type: String },
   id: { type: String, required: true, unique: true }, // Ensure the id is unique
-  time: { type: String, required: true },
-  date: { type: String, required: true },
-  patient: { type: String, required: true },
+  time: { type: String},
+  date: { type: String },
+  patient: { type: String},
 });
 
 // You can remove the _id generation if you don't want to use it, but it's good practice to keep it.
