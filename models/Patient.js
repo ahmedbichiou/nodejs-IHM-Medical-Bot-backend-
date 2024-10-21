d3b192700cbcfca0e585b438a894ad7d9f2b90a4
ahmedbichiou <eunabs@outlook.fr>
2024-10-21 20:51:13 +0100

    backend updates

--- models/Patient.js
+++ models/Patient.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const PatientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   id: { type: String, required: true, unique: true },
+  descirption: { type: String},
 });
 
 const Patient = mongoose.model('Patient', PatientSchema);
